8caa8c7c2942ea69e81a82c79de82865c27da2b7
Thiago Borba <thiagoborba8@gmail.com>
2023-02-07 20:28:46 -0300

    Adicionar arquivos de projeto.

---
+++ LivrariaControleEmprestimo.DATA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LivrariaControleEmprestimo.DATA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
